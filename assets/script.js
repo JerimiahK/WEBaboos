af8eb07b17203313ff150677f36eb87dd80d0846
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2022-11-09 14:03:21 -0700

    added JS to the project to make it functional

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+const button = $("#btn");
+const quoteContainer = $("#quote-container");
+const info = $("#info-container");
+const characterName = $("#character-name");
+const quoteText = $("#quote");
+
+button.on("click", function () {
+  let userInput = $("#search-bar").val();
+  $.ajax({
+    url: "https://animechan.vercel.app/api/random/anime?title=" + userInput,
+  }).then(function (res) {
+    const character = res.character;
+    const quote = res.quote;
+    characterName.text("-" + character);
+    quoteText.text(quote);
+    console.log(res);
+  });
+  $.ajax({
+    url: "https://imdb-api.com/en/API/SearchSeries/k_bdu7lhul/" + userInput,
+  }).then(function (res) {
+    const image = res.results[0].image;
+    $("#image").attr("src", image);
+    console.log(res);
+  });
+});
