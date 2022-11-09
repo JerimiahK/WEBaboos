83df52e4adfcaacead35f971b42c32f744f6a58d
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2022-11-09 14:13:58 -0700

    added keypress event in JS for 'enter' button

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ const quoteContainer = $("#quote-container");
 const info = $("#info-container");
 const characterName = $("#character-name");
 const quoteText = $("#quote");
+const searchBar = $("#search-bar");
 
 button.on("click", function () {
   let userInput = $("#search-bar").val();
@@ -22,4 +23,26 @@ button.on("click", function () {
     $("#image").attr("src", image);
     console.log(res);
   });
+});
+
+searchBar.on("keypress", function (event) {
+  if (event.key === "Enter") {
+    let userInput = $("#search-bar").val();
+    $.ajax({
+      url: "https://animechan.vercel.app/api/random/anime?title=" + userInput,
+    }).then(function (res) {
+      const character = res.character;
+      const quote = res.quote;
+      characterName.text("-" + character);
+      quoteText.text(quote);
+      console.log(res);
+    });
+    $.ajax({
+      url: "https://imdb-api.com/en/API/SearchSeries/k_bdu7lhul/" + userInput,
+    }).then(function (res) {
+      const image = res.results[0].image;
+      $("#image").attr("src", image);
+      console.log(res);
+    });
+  }
 });
